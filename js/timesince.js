c1fa86b933826ad61026f36e63a3d71f739e35d4
begakens <ads57@hotmail.com>
2016-08-21 20:24:39 +0100

    timesince

---
+++ js/timesince.js
@@ -0,0 +1,31 @@
+function timeSince() 
+{
+	var date = new Date("Jun 26, 2016 12:30:00");
+
+    var seconds = Math.floor((new Date() - date) / 1000);
+
+    var interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) {
+        return interval + " years";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+        return interval + " months";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+        return interval + " days";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+        return interval + " hours";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+        return interval + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
+	
+	document.getElementById('countdown').innerHTML = months + ' months ' + days + ' days ' + hours + ' hour ' + minutes + ' minutes';
+}
